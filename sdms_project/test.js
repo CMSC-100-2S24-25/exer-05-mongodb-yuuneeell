1a04ce463b866786266ad3248fa216f6f4b975a9
yuuneeell <ecjoyosa@up.edu.ph>
2025-03-12 04:26:53 +0800

    chore: modified test.js file

--- sdms_project/test.js
+++ sdms_project/test.js
@@ -6,22 +6,10 @@ const saveStudentURL = `http://localhost:3000/save-student`;
 const updateURL = `http://localhost:3000/update`;
 const removeUserURL = `http://localhost:3000/remove-user`;
 const purgeDatabaseURL = `http://localhost:3000/remove-all-user`;
-const getUserURL = `http://localhost:3000/user?stdnum='201954321'`;
-const getAllURL = `http://localhost:3000/members`;
 
-// TEST GET FUNCTIONS-----------
-// searches he user by the student number
-needle.get(getUserURL, (err, res) => {
-    console.log(res.body);
-})
+// TEST POST FUNCTIONS ----------- (comment out after first run of this script!!! otherwise, there will be duplicates)
 
-// gets all the members of the database
-needle.get(getAllURL, (err, res) => {
-    console.log(res.body);
-})
-
-// TEST POST FUNCTIONS -----------
-// SAVING STUDENTS POST
+// SAVE STUDENT
 // student 1
 needle.post(
     saveStudentURL,
@@ -69,8 +57,8 @@ needle.post(
     saveStudentURL,
     {
         stdnum: "202123456",
-        fname: "Timmy Turner",
-        lname: "Watson",
+        fname: "Timmy",
+        lname: "Turner",
         age: "22"
     },
     (err, res) => {
@@ -92,36 +80,65 @@ needle.post(
     }
 );
 
-// UPDATING
-needle.post(
-    updateURL,
-    {
-        searchFName: "Mary Jane",
-        updateLName: "Parker"
-    },
-    (err, res) => {
-        console.log(res.body);
-    }
-);
+// // UPDATING ----------------- (uncomment when necessary)
+// needle.post(
+//     updateURL,
+//     {
+//         searchFName: "Mary Jane",
+//         updateLName: "Parker"
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
 
-// REMOVING USER
-needle.post(
-    removeUserURL,
-    {
-        stdnum: "202123456"
-    },
-    (err, res) => {
-        console.log(res.body);
-    }
-);
+// check if lname updated (uncomment get function below to check):
+// needle.get(`http://localhost:3000/user?stdnum=201876543`,
+//     (err, res) => {
+//         console.log(`GET USER:`, res.body);
+//     })
+
+
+// TEST GET FUNCTIONS----------- (uncomment when needed in testing, you can modify however you want as well)
+
+// // searches the user by the student number
+// needle.get(`http://localhost:3000/user?stdnum=201954321`,
+//     (err, res) => {
+//         console.log(`GET USER:`, res.body);
+//     })
+
+// // searches the user by the student number (should return empty array)
+// needle.get(`http://localhost:3000/user?stdnum=201954353`,
+//     (err, res) => {
+//         console.log(`GET USER:`, res.body);
+//     })
+
+// // gets all the members of the database
+// needle.get(`http://localhost:3000/members`,
+//     (err, res) => {
+//         console.log(`GET ALL:`, res.body);
+//     })
+
+// REMOVE TESTING ------------------- (uncomment when necessary)
+
+// // REMOVING USER
+// needle.post(
+//     removeUserURL,
+//     {
+//         stdnum: "202123456"
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
 
 // REMOVE ALL USERS
-needle.post(
-    purgeDatabaseURL,
-    {},
-    (err, res) => {
-        console.log(res.body);
-    }
-);
+// needle.post(
+//     purgeDatabaseURL,
+//     {},
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// );
 
 
